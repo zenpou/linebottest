ed856af1c93de1360dcd741d2b2dfd9b5f1dc690
takase yuichi <takase.yuichi@kyodo-d.jp>
2018-02-08 18:10:17 +0900

    chat commit

--- app_main.rb
+++ app_main.rb
@@ -39,17 +39,9 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        puts event.message['text']
+        p event
         post_chatwork_api(event.message['text'])
-#         message = {
-#           type: 'text',
-#           text: event.message['text']
-#         }
-#         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-#         response = client.get_message_content(event.message['id'])
-#         tf = Tempfile.open("content")
-#         tf.write(response.body)
       end
     end
   }
